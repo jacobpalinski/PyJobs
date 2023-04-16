95c7ccbe2549b6ce5909108a91aa75464370e822
Kuba Palinski <kpalinski99@gmail.com>
2023-04-16 10:03:19 +0800

    jobDataResource post complete

--- Backend/views.py
+++ Backend/views.py
@@ -12,7 +12,15 @@ class jobDataResource(Resource):
     def get(self):
         ''' Retrieves jobs list based on request parameters specified in get request / filters on search page.
         Must be able to retrieve jobs based on parameters that take a list as a value eg. 'Programming Languages': ['Python','TypeScript']'''
-        pass
+        # Extract arguments from get request
+        location = request.args.get('location')
+        industry = request.args.get('industry')
+        group = request.args.get('group')
+        programming_languages = request.args.getlist('language')
+        databases = request.args.getlist('database')
+        cloud_providers = request.args.getlist('cloud')
+        
+
     def post(self):
         ''' Uses Lambda function to scrape data using linkedin_scraper.py script, checking for existing data in database,
         removing duplicates from new data, and inserts new data into database'''
@@ -38,6 +46,22 @@ class jobDataResource(Resource):
                     job_details_url = linkedin_scraper.generate_url.generate_job_details_url(job_id)
                     job_details_html = linkedin_scraper.html_retriever.get_html(job_details_url)
                     linkedin_scraper.extract_job_data(job_id,job_details_html)
+            
+        # Check if job_id for job is in jobData database
+        for job in linkedin_scraper.job_data:
+            if jobData.objects(jobId = job['Job_Id']):
+                continue
+            # Append new job to jobData database
+            try:
+                new_job = jobData(jobID = job['Job_Id'], company = job['Company'], location = job['Location'], industry = job['Industry'], jobTitle = job['Job Title'],
+                group = job['Group'], programmingLanguages = job['Programming Languages'], databases = job['Databases'], cloudProviders = job['Cloud Providers'],
+                link = job['Link'], datePosted = job['Date Posted'])
+                new_job.validate()
+                new_job.save()
+            except Exception as e:
+                response = {'message': e}
+                return response, HttpStatus.bad_request_400.value
+        return {'message': 'New jobs added successfully'}, HttpStatus.created_201.value
 
     def patch(self):
         ''' Updates jobData based on parameters specified in request.'''
