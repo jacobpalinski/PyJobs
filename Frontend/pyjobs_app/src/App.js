6eed0672105859144b51b5a74b64a1603afa0060
Kuba Palinski <kpalinski99@gmail.com>
2023-07-06 06:40:19 +0800

    sign up functionality

--- Frontend/pyjobs_app/src/App.js
+++ Frontend/pyjobs_app/src/App.js
@@ -6,30 +6,46 @@ import LoginSignUp from "./path" */
 
 function App() {
 	const initialValues = { username: "", password: ""}
-	const [formValues, setFormValues] = useState(initialValues);
-	const [formErrors, setFormErrors] = useState({});
-	const [isSubmit, setIsSubmit] = useState(false);
+	const [signInFormValues, setSignInFormValues] = useState(initialValues);
+	const [signUpFormValues, setSignUpFormValues] = useState(initialValues);
+	const [signInFormErrors, setSignInFormErrors] = useState({});
+	const [signUpFormErrors, setSignUpFormErrors] = useState({});
+	const [signInSubmit, setSignInSubmit] = useState(false);
+	const [signUpSubmit, setSignUpSubmit] = useState(false);
 	const [login, setLogin] = useState(true);
+	const [registered, setRegistered] = useState(false);
 	const [isSignUpActive, setIsSignUpActive] = useState(false);
 
 	// const navigate = useNavigate();
 
-	const handleChange = (e) => {
+	const handleSignInChange = (e) => {
 		const {name, value} = e.target;
-		setFormValues({...formValues, [name]: value});
-		console.log(formValues);
+		setSignInFormValues({...signInFormValues, [name]: value});
+		console.log(signInFormValues);
 	};
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		setFormErrors(validate(formValues));
-		setIsSubmit(true);
+	const handleSignUpChange = (e) => {
+		const {name, value} = e.target;
+		setSignUpFormValues({...signUpFormValues, [name]: value});
+		console.log(signUpFormValues);
 	};
+
+	const signIn = (e) => {
+		e.preventDefault();
+		setSignInFormErrors(validateSignIn(signInFormValues));
+		setSignInSubmit(true);
+	};
+
+	const signUp = (e) => {
+		e.preventDefault();
+		setSignUpFormErrors(validateSignUp(signUpFormValues));
+		setSignUpSubmit(true);
+	}
 
 	useEffect(() => {
-		console.log(formErrors);
-		if(Object.keys(formErrors).length === 0 && isSubmit) {
-			console.log(formValues);
+		console.log(signInFormErrors);
+		if(Object.keys(signInFormErrors).length === 0 && signInSubmit) {
+			console.log(signInFormValues);
 			const fetchLogin = async () => {
 				try {
 					const response = await fetch('http://127.0.0.1:5000/job_data/user/login',{
@@ -37,7 +53,7 @@ function App() {
 						headers: {
 							'Content-Type': 'application/json'
 						},
-						body: JSON.stringify(formValues)
+						body: JSON.stringify(signInFormValues)
 					});
 					if (response.ok) {
 						console.log('Successful login');
@@ -54,15 +70,79 @@ function App() {
 			};
 			fetchLogin();
 		}
-	}, [formErrors, formValues, isSubmit])
+	}, [signInFormErrors, signInFormValues, signInSubmit]);
 
-	const validate = (values) => {
+	useEffect(() => {
+		console.log(signUpFormErrors);
+		if (Object.keys(signUpFormErrors).length === 0 && signUpSubmit) {
+			console.log(signUpFormValues);
+			const fetchSignUp = async () => {
+				try {
+					const response = await fetch('http://127.0.0.1:5000/job_data/user',{
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json'
+						},
+						body: JSON.stringify(signUpFormValues)
+					});
+					if (response.ok) {
+						setRegistered(true);
+						console.log('Successful Sign Up');
+					} else {
+						const errorData = await response.json();
+						console.log(errorData);
+					}
+	
+				} catch (error) {
+					console.error('Error during login:', error);
+				}
+			};
+			fetchSignUp();
+		}
+	}, [signUpFormErrors, signUpFormValues, signUpSubmit])
+
+	const validateSignIn = (values) => {
 		const errors = {};
 		if (!values.username) {
 			errors.username = "Username is required";
 		};
 		if (!values.password) {
 			errors.password = "Password is required";
+		};
+		return errors;
+	};
+
+	const validateSignUp = (values) => {
+		const errors = {};
+		const checkDigit = /\d/;
+		const checkUppercaseLetter = /[A-Z]/;
+		const checkLowercaseLetter = /[a-z]/;
+		const checkSpecialCharacters = /[ !#$%&'()*+,-./[\\\]^_`{|}~"+r'"]/; 
+
+		if (!values.username) {
+			errors.username = "Username is required";
+		} else if (values.username.length < 5) {
+			errors.username = "Username must have at least 5 characters";
+		} else if (!checkUppercaseLetter.test(values.username) && !checkLowercaseLetter.test(values.username)) {
+			errors.username = "Username must contain at least 1 letter";
+		} else if (!checkDigit.test(values.username)){
+			errors.username = "Username must contain at least 1 digit";
+		}
+
+		if (!values.password) {
+			errors.password = "Password is required";
+		} else if (values.password.length < 8) {
+			errors.password = "Password must have at least 8 characters";
+		} else if (values.password.length > 32) {
+			errors.password = "Password must be less than 32 characters";
+		} else if (!checkUppercaseLetter.test(values.password)) {
+			errors.password = "Password must contain at least 1 uppercase letter";
+		} else if (!checkLowercaseLetter.test(values.password)) {
+			errors.password = "Password must contain at least 1 lowercase letter";
+		} else if (!checkDigit.test(values.password)){
+			errors.password = "Password must contain at least 1 digit";
+		} else if (!checkSpecialCharacters.test(values.password)) {
+			errors.password = "Password must contain at least 1 special character";
 		}
 		return errors;
 	}
@@ -90,22 +170,23 @@ function App() {
 </div>
 <div className={`container ${isSignUpActive ? 'right-panel-active' : ''}`}>
 	<div className="form-container sign-up-container">
-		<form onSubmit = {handleSubmit}>
+		<form onSubmit = {signUp}>
 			<h1>Create Account</h1>
-			<input type="text" name="username" value={formValues.username} onChange={handleChange} placeholder="Username" />
-			<p>{formErrors.username}</p>
-			<input type="password" name="password" value={formValues.password} onChange={handleChange} placeholder="Password" />
-			<p>{formErrors.password}</p>
+			<input type="text" name="username" value={signUpFormValues.username} onChange={handleSignUpChange} placeholder="Username" />
+			<p>{signUpFormErrors.username}</p>
+			<input type="password" name="password" value={signUpFormValues.password} onChange={handleSignUpChange} placeholder="Password" />
+			<p>{signUpFormErrors.password}</p>
 			<button>Sign Up</button>
+			{registered && <p>Sign Up Successful. Login with Credentials</p>}
 		</form>
 	</div>
 	<div className="form-container sign-in-container">
-		<form onSubmit = {handleSubmit}>
+		<form onSubmit = {signIn}>
 			<h1>Sign in</h1>
-			<input type="text" name="username" value={formValues.username} onChange={handleChange} placeholder="Username"/>
-			<p>{formErrors.username}</p>
-			<input type="password" name="password" value={formValues.password} onChange={handleChange} placeholder="Password" />
-			<p>{formErrors.password}</p>
+			<input type="text" name="username" value={signInFormValues.username} onChange={handleSignInChange} placeholder="Username"/>
+			<p>{signInFormErrors.username}</p>
+			<input type="password" name="password" value={signInFormValues.password} onChange={handleSignInChange} placeholder="Password" />
+			<p>{signInFormErrors.password}</p>
 			<button>Sign In</button>
 			{!login && <p>Invalid Credentials. Try Again</p>}
 		</form>
